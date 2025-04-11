f1cda3d99f18210eaf301f7c9011a9df06a431d9
mehazzz <153672515+mehazzz@users.noreply.github.com>
2025-04-11 07:50:25 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    base: "/edusync/",
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'lcov'],
+      // vite.config.js
+// test: {
+//   globals: true,
+//   environment: 'jsdom',
+//   setupFiles: './src/setupTests.js',
+// }
+
+
+
+    },
+  },
+})
+
